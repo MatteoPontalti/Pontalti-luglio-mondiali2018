74b06f72a5b5340a76b4e412d56329189711a04d
MatteoPontalti <matteo.pontalti@studenti.unitn.it>
2018-08-21 10:36:22 +0200

    Esama Agosto aggiornato

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Content-Type', 'application/json');
   if (req.method == 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET', 'POST', PUT);
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT');
     //return res.status(200).json({});
   }
   next();
